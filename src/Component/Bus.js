d560063f9c0cba40d60840bd6090139b11bd6c59
young <young@PIM.AC.TH>
2018-01-06 13:23:17 +0700

    add responsive

--- src/Component/Bus.js
+++ src/Component/Bus.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { Tabs, Button } from 'antd';
+const TabPane = Tabs.TabPane;
+const ButtonGroup = Button.Group;
 class Bus extends Component{
     
     constructor(props) {
@@ -11,7 +14,60 @@ class Bus extends Component{
 
     render(){
         return(
-            <div></div>
+            <div style={{backgroundColor:"white"}}>
+                <div style={{height: "10vw"}}>
+
+                </div>
+              <div style={{ margin: "auto",
+                            width: "50vw",
+                            height: '30vh',
+                            backgroundColor: "orange"}}>
+              </div>
+              <div style={{textAlign:"center"}}>
+                
+                <br/>
+                <h3>   บริษัทรถทัวร์ : อิอิ</h3>
+                <br/>
+                <h3>   ราคา : 10000 baht</h3>
+              </div>
+              <div style={{display: 'flex', justifyContent: 'center'}}>
+                 
+              <Button type="primary">Back</Button>
+              <span style={{color:"white"}}>asdasdasd</span>
+              <Button type="primary">Select</Button>
+                 
+              </div>
+              <div >
+              <Tabs defaultActiveKey="1" type="card" style={{padding: "20px"}}>
+                <TabPane tab="รายละเอียด" key="1">
+                    <h1>Content of Tab Pane 1</h1>
+                    <h1>Content of Tab Pane 1</h1>
+                    <h1>Content of Tab Pane 1</h1>
+                    <h1>Content of Tab Pane 1</h1>
+                    <h1>Content of Tab Pane 1</h1>
+                    <h1>Content of Tab Pane 1</h1>
+                    <h1>Content of Tab Pane 1</h1>
+                    <h1>Content of Tab Pane 1</h1>
+                    <h1>Content of Tab Pane 1</h1>
+                    <h1>Content of Tab Pane 1</h1>
+                    <h1>Content of Tab Pane 1</h1>
+                </TabPane>
+                <TabPane tab="Review" key="2">
+                <h1>Content of Tab Pane 1</h1>
+                    <h1>Content of Tab Pane 2</h1>
+                    <h1>Content of Tab Pane 2</h1>
+                    <h1>Content of Tab Pane 2</h1>
+                    <h1>Content of Tab Pane 2</h1>
+                    <h1>Content of Tab Pane 1</h1>
+                    <h1>Content of Tab Pane 1</h1>
+                    <h1>Content of Tab Pane 1</h1>
+                    <h1>Content of Tab Pane 1</h1>
+                    <h1>Content of Tab Pane 1</h1>
+                    <h1>Content of Tab Pane 1</h1>
+                </TabPane>
+            </Tabs>
+              </div>
+            </div>
         )
     }
 
--- src/Component/SearchBar.js
+++ src/Component/SearchBar.js
@@ -12,54 +12,61 @@ class SearchBar extends Component{
         return(
             <div style={{backgroundColor:"pink",
                         width: "100vw",
-                        height: '32vh',}}>
+                        }}>
                 <div>
                     <Checkbox>ที่พัก</Checkbox>
                     <Row >
-                        <Col span={3} offset={1} >สถานที่</Col>
-                        <Col span={3} offset={1} >เช็คอิน</Col>
-                        <Col span={3} offset={1} >checkout</Col>
-                        <Col span={3} offset={1} >ผู้เข้าพัก</Col>
-                        <Col span={3} offset={1} >เงิน</Col>
+                        
+                        <Col xs={12} sm={12} md={3} lg={3} span={3} offset={1} >
+                        <div>สถานที่</div>
+                        <div><Input onChange={(e) => 
+                            { this.props.changefilter_Resort_Place(e.target.value); }}/></div></Col>
+                        <Col xs={12} sm={12} md={3} lg={3} span={3} offset={1} >
+                        <div>เช็คอิน</div>
+                        <div><Input onChange={(e) => 
+                            { this.props.changefilter_Resort_Checkin(e.target.value); }}/></div>
+                        </Col>
+                        <Col xs={12} sm={12} md={3} lg={3} span={3} offset={1} >
+                        <div>checkout</div>
+                        <div><Input onChange={(e) => 
+                            { this.props.changefilter_Resort_Checkout(e.target.value); }}/></div></Col>
+                        <Col xs={12} sm={12} md={3} lg={3} span={3} offset={1} >
+                        <div>ผู้เข้าพัก</div>
+                        <div><Input onChange={(e) => 
+                            { this.props.changefilter_Resort_Personal(e.target.value); }}/></div></Col>
+                        <Col xs={12} sm={12} md={3} lg={3} span={3} offset={1} >
+                        <div>เงิน</div>
+                        <div><Input onChange={(e) => 
+                            { this.props.changefilter_Resort_Money(e.target.value); }}/></div></Col>
                     </Row>
-
-                    
-                    <Row>
-                        <Col span={3} offset={1} ><Input onChange={(e) => 
-                            { this.props.changefilter_Resort_Place(e.target.value); }}/></Col>
-                        <Col span={3} offset={1} ><Input onChange={(e) => 
-                            { this.props.changefilter_Resort_Checkin(e.target.value); }}/></Col>
-                        <Col span={3} offset={1} ><Input onChange={(e) => 
-                            { this.props.changefilter_Resort_Checkout(e.target.value); }}/></Col>
-                        <Col span={3} offset={1} ><Input onChange={(e) => 
-                            { this.props.changefilter_Resort_Personal(e.target.value); }}/></Col>
-                        <Col span={3} offset={1} ><Input onChange={(e) => 
-                            { this.props.changefilter_Resort_Money(e.target.value); }}/></Col>
-                    </Row>
-
+                    <div style={{height:"40px"}}></div>
+                    <div style={{offsetLeft:"100px"}}>
                     <Checkbox>รถทัวร์</Checkbox>
+                    </div>
                     <Row >
-                        <Col span={3} offset={1}>ต้นทาง</Col>
-                        <Col span={3} offset={1}>ปลายทาง</Col>
-                        <Col span={3} offset={1}>วันออกเดินทาง</Col>
-                        <Col span={3} offset={1}>วันกลับ</Col>
-                        <Col span={3} offset={1}>จำนวนผู้โดยสาร</Col>
+                        <Col xs={12} sm={12} md={3} lg={3} span={3} span={3} offset={1}>
+                        <div>ต้นทาง</div>
+                        <div><Input onChange={(e) => 
+                            { this.props.changefilter_Bus_from(e.target.value); }}/></div></Col>
+                        <Col xs={12} sm={12} md={3} lg={3} span={3} span={3} offset={1}>
+                        <div>ปลายทาง</div>
+                        <div><Input onChange={(e) => 
+                            { this.props.changefilter_Bus_to(e.target.value); }}/></div></Col>
+                        <Col xs={12} sm={12} md={3} lg={3} span={3} span={3} offset={1}>
+                        <div>วันออกเดินทาง</div>
+                        <div><Input onChange={(e) => 
+                            { this.props.changefilter_Bus_date_going(e.target.value); }}/></div></Col>
+                        <Col xs={12} sm={12} md={3} lg={3} span={3} span={3} offset={1}>
+                        <div>วันกลับ</div>
+                        <div><Input onChange={(e) => 
+                            { this.props.changefilter_Bus_Goback(e.target.value); }}/></div></Col>
+                        <Col xs={12} sm={12} md={3} lg={3} span={3} span={3} offset={1}>
+                        <div>จำนวนผู้โดยสาร</div>
+                        <div><Input onChange={(e) => 
+                            { this.props.changefilter_Bus_Number(e.target.value); }}/></div></Col>
   
                     </Row>
 
-                    <Row>
-                        <Col span={3} offset={1} ><Input onChange={(e) => 
-                            { this.props.changefilter_Bus_from(e.target.value); }}/></Col>
-                        <Col span={3} offset={1} ><Input onChange={(e) => 
-                            { this.props.changefilter_Bus_to(e.target.value); }}/></Col>
-                        <Col span={3} offset={1} ><Input onChange={(e) => 
-                            { this.props.changefilter_Bus_date_going(e.target.value); }}/></Col>
-                        <Col span={3} offset={1} ><Input onChange={(e) => 
-                            { this.props.changefilter_Bus_Goback(e.target.value); }}/></Col>
-                        <Col span={3} offset={1} ><Input onChange={(e) => 
-                            { this.props.changefilter_Bus_Number(e.target.value); }}/></Col>
-                        
-                    </Row>
                     <div style={{height:"1vh"}}></div>
                     <Row>
                         <Col span={3} offset={1}><Button type="primary">Search</Button></Col>
