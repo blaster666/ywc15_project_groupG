a6a2fbd9c89f02bf44a6b0dde21d6078dc7edb6b
young <young@PIM.AC.TH>
2018-01-06 14:52:59 +0700

    add location

--- src/Component/SearchBar1.js
+++ src/Component/SearchBar1.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Form, Row, Col, Input, Button, Icon, Checkbox } from 'antd';
-
+import { Select } from 'antd';
+const Option = Select.Option;
 class SearchBar1 extends Component{
 
     render(){
@@ -8,9 +9,22 @@ class SearchBar1 extends Component{
             <div>
                 <div>
                     <div style={{width:"60%", margin:"auto"}}>
+                    <center><Select defaultValue="location"  >
+                    <Option value="1">หัวหิน</Option>
+                    <Option value="2">ระยอง</Option>
+                    <Option value="3">ปราณบุรี</Option>
+                    <Option value="4">กาญจนบุรี</Option>
+                    <Option value="5">ชะอำ</Option>
+                    </Select></center>
+                    </div>
+                    <br />
+
+                    <div style={{width:"60%", margin:"auto"}}>
                     <Input placeholder="Money" size="large" onChange={(e) => 
                             { this.props.changeBudget(e.target.value); }}></Input>
                     </div>
+                    
+                    
                     <br />
                     <div style={{display: 'flex', justifyContent: 'center'}}>
                     <Button type="primary">ต่อไป</Button>
