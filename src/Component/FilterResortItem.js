c9c63a7bbd664a622bcf82ea83d93dad64dc1c59
Udomsub Naikorn <udomsub.nk@gmail.com>
2018-01-06 11:32:14 +0700

    Show Fitlered resort items

--- src/Component/FilterResortItem.js
+++ src/Component/FilterResortItem.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
 class FilterResortItem extends Component{
-
+    componentWillReceiveProps(nextProps) {
+        this.forceUpdate();
+    }
     constructor(props) {
         super(props);
-  
-      }
+    }
 
     render(){
         return(
-            <div style={{color:'red'}}>
-                <div>FilterResortItem</div>
+            <div style={{
+                backgroundColor:'black',width:'150px',height:'100px',display:'inline-block',marginLeft:'20px'
+                }}>
+                
             </div>
         )
     }
