d6b1bb6f63d90be5dbbc217a12b35f365b5ab12b
Udomsub Naikorn <udomsub.nk@gmail.com>
2018-01-06 11:08:28 +0700

    Select to show FilterPage

--- src/Component/FilterPage.js
+++ src/Component/FilterPage.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react'
-import FilterPageBus from './FilterPageBus';
 import FilterPageResort from './FilterPageResort';
+import FilterPageBus from './FilterPageBus';
+
 class FilterPage extends Component{
-    
     constructor(props) {
         super(props);
-  
-      }
-
+        this.state={
+            ShowPage : 'resort'
+        }
+        this.ShowFilterPageResort = this.ShowFilterPageResort.bind(this)
+        this.ShowFilterPageBus = this.ShowFilterPageBus.bind(this)
+    }
+    ShowFilterPageResort(){
+        this.setState({
+            ShowPage : 'resort'
+        })
+    }
+    ShowFilterPageBus(){
+        this.setState({
+            ShowPage : 'bus'
+        })
+    }
     render(){
         return(
             <div style={{
-                backgroundColor:"yellow",  
-                width: "100vw",
+                    backgroundColor:"yellow",  
+                    width: "100vw",
                 }}>
                 
-                <FilterPageBus></FilterPageBus>
-                <FilterPageResort></FilterPageResort>
+                <button onClick={this.ShowFilterPageResort}>Resort</button>
+                <button onClick={this.ShowFilterPageBus}>Bus</button>
+
+                {this.state.ShowPage=='resort'?<FilterPageResort></FilterPageResort>:<FilterPageBus></FilterPageBus>}
             </div>
         )
     }
---
+++ src/Component/FilterResortItem.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+class FilterResortItem extends Component{
+
+    constructor(props) {
+        super(props);
+  
+      }
+
+    render(){
+        return(
+            <div style={{color:'red'}}>
+                <div>FilterResortItem</div>
+            </div>
+        )
+    }
+
+}
+export default FilterResortItem
